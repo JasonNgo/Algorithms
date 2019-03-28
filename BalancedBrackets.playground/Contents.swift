f77bd0a80f9978e619b7632c5bb0a4530207a218
Jason Ngo <jason.n.ngo@gmail.com>
2019-03-28 17:14:10 -0400

    Completed BalancedBracket Question

---
+++ BalancedBrackets.playground/Contents.swift
@@ -0,0 +1,59 @@
+/**
+ *  Problem: Determine if a given string of brackets is balanced
+ **/
+
+import Cocoa
+
+enum Bracket: Character {
+    case paraenthesis = "("
+    case square = "["
+    case curly = "{"
+}
+
+public struct Stack<Value> {
+    private var storage: [Value] = []
+    
+    var isEmpty: Bool {
+        return storage.isEmpty
+    }
+    
+    func peek() -> Value? {
+        return storage.last
+    }
+        
+    mutating func push(_ value: Value) {
+        storage.append(value)
+    }
+    
+    @discardableResult
+    mutating func pop() -> Value? {
+        return storage.popLast()
+    }
+}
+
+func balancedBrackets(input: String) -> Bool {
+    guard !input.isEmpty else {
+        return true
+    }
+    
+    var stack = Stack<Character>()
+    
+    for char in input {
+        guard let top = stack.peek() else {
+            stack.push(char)
+            continue
+        }
+        
+        if top == "(" && char == ")" || top == "{" && char == "}" || top == "[" && char == "]" {
+            stack.pop()
+        } else {
+            stack.push(char)
+        }
+    }
+    
+    return stack.isEmpty
+}
+
+assert(balancedBrackets(input: "()[]{}{") == false)
+assert(balancedBrackets(input: "()()[{()})]") == false)
+assert(balancedBrackets(input: "([])(){}(())()()") == true)
