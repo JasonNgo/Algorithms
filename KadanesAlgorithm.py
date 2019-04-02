624ed59abdaa062258598bfe590e7ddb7b236f39
Jason Ngo <jason.n.ngo@gmail.com>
2019-04-02 15:52:47 -0400

    completed

---
+++ KadanesAlgorithm.py
@@ -0,0 +1,11 @@
+# Kadane's Algorithm: Largest sum subarray
+
+def kadanesAlgorithm(array):
+    maxEndingHere = array[0]
+    maxSoFar = array[0]
+    for i in range(1, len(array)):
+        num = array[i]
+        maxEndingHere = max(num, maxEndingHere + num)
+        maxSoFar = max(maxEndingHere, maxSoFar)
+
+    return maxSoFar
